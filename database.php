ec0d8f8f26872d8ac26d09e0f9ab112fea6779e0
= ChrisKomarov <=chriskomarov@gmail.com>
2017-12-11 22:01:17 -0500

    changes up to 50min, some errors

--- database.php
+++ database.php
@@ -1,6 +1,53 @@
 <?php
-
-
 class Database{
-	
+	public $conn = null;
+
+	public function getConnection(){
+		if(!isset($conn)|| is_null($conn)){
+				$this->conn = new PDO('sqlite:./flowers.db');
+		}
+	}
+	public function getFlowers (){
+		$this->getConnection();
+		$sql = "SELECT * FROM FLOWERS ORDER BY COMNAME";
+		$results = $this->conn->query($sql);
+		$results_array = array();
+		foreach($results as $row){
+			$cur_row = array();
+			$cur_row['comname'] = $row['COMNAME'];
+			$cur_row['genus'] = $row['GENUS'];
+			$cur_row['species'] = $row['SPECIES'];
+			$results_array[] = $cur_row;
+		}
+		return $results_array;
+	}
+	public function getSightings($flower){
+		$this->getConnection();
+		$sql = "SELECT * FROM SIGHTINGS WHERE NAME = :flower";
+		$stmt = $this->conn->prepare($sql);
+		if($stmt === False){
+			return null;
+		}
+		$stmt->execute(array(":flower" -> $flower));
+		$results = $stmt -> fetchAll();
+		$results_array = array();
+		foreach($results as $row){
+			$cur_row = array();
+			$cur_row['name'] = $row['NAME'];
+			$cur_row['person'] = $row['PERSON'];
+			$cur_row['location'] = $row['LOCATION'];
+			$cur_row['sighted'] = $row['SIGHTED'];
+			$results_array[] = $cur_row;
+		}
+		return $results_array;
+	}
+	public function updateFlowers($flower, $genus, $species, $comname){
+
+	}
+	public function addSighting($flower, $person, $location, $sighted){
+
+	}
+	public function close(){
+		$this->conn = null;
+	}
 }
--- index.php
+++ index.php
@@ -1,17 +1,48 @@
-<?php require 'header.php' ?>
+<?php require 'header.php';
+  require 'database.php';
+
+  $db = new Database();
+  $flowers = $db->getFlowers();
+  $message = null;
+//something is going wrong in here
+
+  if(isset($_GET) && isset($_GET['flower'])){
+    if($_GET['flower'] == -1){
+      $message = "Please select a flower";
+    }
+    else {
+      $flower = $_GET['flower'];
+      $sightings = $db->getSightings($flower);
+    }
+  }
+//until here. It seems to be that it never really goes to the else statement properly or by default?
+?>
 
     <div class="container">
+      <?php
+        if(!is_null ($message)){
+          echo('<div class = "panel panel-default"><div class = "bg-danger panel-body">' .$message.'</div></div><br>');
+        }
+      ?>
 
-        <form class="form-inline">
-		  <div class="form-group">
-			<label for="flower">Flower</label>
-			<select class="form-control" id="flower" ></select>
-		  </div>
-		  
+        <form class="form-inline" method = "get">
+    		  <div class="form-group">
+      			<label for="flower">Flower</label>
+      			<select class="form-control" id="flower"  name = "flower">
+              <option value = "-1">Select One</option>
+              <?php
+                  foreach($flowers as $f){  ?>
+                    <option value ="<?= $f["comname"]?>"><?php echo $f["comname"];?></option>
+                  <?php
+                    }
+               ?>
+            </select>
+    		  </div>
+
 		  <button type="submit" class="btn btn-default">Go</button>
 		</form>
 		<hr>
-		<h2>Sightings for %%Make Flower Show here%%</h2>
+		<h2>Sightings for <?= $flower ?></h2>
 		<table class="table table-striped">
 			<tr>
 				<th>Name</th>
@@ -19,7 +50,18 @@
 				<th>Location</th>
 				<th>Sighted</th>
 			</tr>
-			
+      <?php
+        foreach($sightings as $s):
+      ?>
+        <tr>
+          <td><?=$s['name']?></td>
+          <td><?=$s['person']?></td>
+          <td><?=$s['location']?></td>
+          <td><?=$s['sighted']?></td>
+        </tr>
+      <?php
+        endforeach;
+       ?>
 		</table>
     </div><!-- /.container -->
 
