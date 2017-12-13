4ac3fa91938880be93ea1c63ee31f12c7d00f332
Ricardo <ricardowagner.op@gmail.com>
2017-12-13 01:40:26 -0500

    Pt 1: Query view - COMPLETED

--- database.php
+++ database.php
@@ -7,6 +7,7 @@ class Database{
 				$this->conn = new PDO('sqlite:./flowers.db');
 		}
 	}
+
 	public function getFlowers (){
 		$this->getConnection();
 		$sql = "SELECT * FROM FLOWERS ORDER BY COMNAME";
@@ -22,7 +23,24 @@ class Database{
 		return $results_array;
 	}
 	public function getSightings($flower){
-
+		$this->getConnection();
+		$sql = "SELECT * FROM SIGHTINGS WHERE NAME = :flower";
+		$stmt = $this->conn->prepare($sql);
+		if($stmt === False){
+			return null;
+		}
+		$stmt->execute(array(':flower' => $flower));
+		$results = $stmt -> fetchAll();
+		$results_array = array();
+		foreach($results as $row){
+			$cur_row = array();
+			$cur_row['name'] = $row['NAME'];
+			$cur_row['person'] = $row['PERSON'];
+			$cur_row['location'] = $row['LOCATION'];
+			$cur_row['sighted'] = $row['SIGHTED'];
+			$results_array[] = $cur_row;
+		}
+		return $results_array;
 	}
 	public function updateFlowers($flower, $genus, $species, $comname){
 
@@ -31,6 +49,6 @@ class Database{
 
 	}
 	public function close(){
-
+		$this->conn = null;
 	}
 }
--- index.php
+++ index.php
@@ -1,15 +1,34 @@
 <?php require 'header.php';
   require 'database.php';
+
   $db = new Database();
   $flowers = $db->getFlowers();
+  $message = null;
+//something is going wrong in here
+
+  if(isset($_GET) && isset($_GET['flower'])){
+    if($_GET['flower'] == -1){
+      $message = "Please select a flower";
+    }
+    else {
+      $flower = $_GET['flower'];
+      $sightings = $db->getSightings($flower);
+    }
+  }
+  $db->close();
 ?>
 
     <div class="container">
+      <?php
+        if(!is_null ($message)){
+          echo('<div class = "panel panel-default"><div class = "bg-danger panel-body">' .$message.'</div></div>');
+        }
+      ?>
 
         <form class="form-inline" method = "get">
     		  <div class="form-group">
       			<label for="flower">Flower</label>
-      			<select class="form-control" id="flower" >
+      			<select class="form-control" id="flower"  name = "flower">
               <option value = "-1">Select One</option>
               <?php
                   foreach($flowers as $f){  ?>
@@ -23,7 +42,8 @@
 		  <button type="submit" class="btn btn-default">Go</button>
 		</form>
 		<hr>
-		<h2>Sightings for %%Make Flower Show here%%</h2>
+    <?php if(isset($sightings)): ?>
+		<h2>Sightings for <?= $flower ?></h2>
 		<table class="table table-striped">
 			<tr>
 				<th>Name</th>
@@ -32,7 +52,20 @@
 				<th>Sighted</th>
 			</tr>
 
+      <?php
+        foreach($sightings as $s):
+      ?>
+        <tr>
+          <td><?=$s['name']?></td>
+          <td><?=$s['person']?></td>
+          <td><?=$s['location']?></td>
+          <td><?=$s['sighted']?></td>
+        </tr>
+      <?php
+    endforeach;
+       ?>
 		</table>
+  <?php endif; ?>
     </div><!-- /.container -->
 
 
